b959513cafbafc48c7e237d7d95de03845302494
Swaakk <swathykrishna.thayyil@gmail.com>
2022-02-17 14:59:02 +0530

    second commit started desigining

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import MenuContainer from "./Components/MenuContainer";
+import pinterest from "./Images/pinterest.png";
+import Person from "@mui/icons-material/Person"
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <div className="menuContainer">
+        <img src={pinterest} className="logo" />
+        
+        <div className="subMenu">
+          <div>
+            <MenuContainer icon={<Person/>}/>
+          </div>
+        </div>
+      </div>
+      <main></main>
     </div>
   );
 }
