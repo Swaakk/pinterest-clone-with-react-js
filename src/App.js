c4bbda1e1fe664d8c9d2dfa6d6e15231126b9ba8
Swaakk <swathykrishna.thayyil@gmail.com>
2022-02-18 18:11:50 +0530

    second commit continueing with designing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
 import MenuContainer from "./Components/MenuContainer";
 import pinterest from "./Images/pinterest.png";
-import Person from "@mui/icons-material/Person"
+import {Person,Notifications,Chat,FavoriteRounded,QuestionMark,Add} from "@mui/icons-material"
+import { useEffect } from "react";
+import icons from './Images/icons.png'
+
 
 function App() {
+
+  useEffect(()=>{
+
+    const allIcon = document.querySelectorAll(".iconContainer");
+
+
+    function setMenuActive(){
+      allIcon.forEach((e)=>e.classList.remove('active'))
+      this.classList.add('active')
+    }
+
+    allIcon.forEach((e)=>e.addEventListener("click", setMenuActive))
+
+  },[]);
+    
   return (
     <div className="App">
       <div className="menuContainer">
@@ -12,10 +30,29 @@ function App() {
         <div className="subMenu">
           <div>
             <MenuContainer icon={<Person/>}/>
+            <MenuContainer icon={<Notifications/>}/>
+            <MenuContainer icon={<Chat/>}/>
+          </div>
+          <div>
+            <MenuContainer icon={<FavoriteRounded/>}/>
+          </div>
+          <div>
+            <MenuContainer icon={<QuestionMark/>}/>
+            <MenuContainer icon={<Add/>}/>
           </div>
         </div>
       </div>
-      <main></main>
+      <main>
+        <div className="searchBox">
+          <input type="text" placeholder="search...." />
+          <div className="search">
+            <img src={icons} alt="" />
+          </div>
+        </div>
+        <div className="mainContainer">
+          
+        </div>
+      </main>
     </div>
   );
 }
--- src/Components/MenuContainer.js
+++ src/Components/MenuContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MenuContainer({icon}) {
   return (
-    <div className='iconContainer'>
+    <div className='iconContainer' >
         {icon}
     </div>
   )
