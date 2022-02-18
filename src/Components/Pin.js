b2f4d7a720be66a39bfc24860aabe4f998327c49
Swaakk <swathykrishna.thayyil@gmail.com>
2022-02-18 19:54:01 +0530

    finilizing desing

--- src/Components/Pin.js
+++ src/Components/Pin.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import icons from '../Images/icons.png'
 
 function Pin({pinSize}) {
   return (
     <div className={`Pin ${pinSize}`}>
         <img className='mainPic' src="https://cdn.pixabay.com/photo/2017/08/06/15/13/woman-2593366_1280.jpg" alt="" />
+        <div className="content">
+            <h1>sample name</h1>
+            <div className='search'>
+                <img src={icons} />
+            </div>
+
+        </div>
     </div>
   )
 }
 
-export default Pin
+export default Pin;
